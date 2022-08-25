4748e556f6d29afa197d668721d11f514f341bf6
Lucas Henrique <lucas.henrique@nstech.com.br>
2022-08-25 14:14:22 -0300

    creates a generic practice file

---
+++ src/generics.ts
@@ -0,0 +1,18 @@
+enum TaskType {
+  feature = 'feature',
+  bug = 'bug',
+}
+
+type Task<T = TaskType> = {
+  name: string;
+  type: T;
+};
+
+const whatever: Task = { name: 'lucas', type: TaskType.feature };
+
+type FeatureTask = Task<TaskType.feature>;
+
+const feature: FeatureTask = { name: 'feature', type: TaskType.feature };
+feature.type = TaskType.bug;
+
+const invalid: FeatureTask = { name: 'invalid', type: TaskType.bug };
